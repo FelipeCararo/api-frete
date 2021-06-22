66937fdd41408ebcf48dee8dd90f3be3792397d1
FelipeCararo <felipe.gorges.cararo@gmail.com>
2021-06-22 18:27:47 -0300

    Ajuste comentários

--- node/src/app/controllers/FreteController.js
+++ node/src/app/controllers/FreteController.js
@@ -59,7 +59,7 @@ class FreteController {
   }
 
   /**
-   * Realiza a criação do user
+   * Realiza o tratamento da quote
    * @param {Object} req
    * @param {Object} res
    */
@@ -140,7 +140,7 @@ class FreteController {
   }
 
   /**
-   * Rotas dos Planos de Venda
+   * Rotas do frete controller
    */
   routes() {
     routes.get('/', this.connect);
